d008b7e3432899c40cf0130d6fddf0d88b5677fe
Rian Battle <89739973+RianBattle@users.noreply.github.com>
2025-07-14 10:57:51 -0400

    Create bisection_method.py

---
+++ bisection_method.py
@@ -0,0 +1,7 @@
+def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
+    """
+    square_target: number for which to find the square root
+    tolerance: acceptable difference between the square of the approximate root and the actual target value
+    max_iterations: the max # of iterations to perform
+    """
+    pass
